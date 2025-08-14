fa8ff92518a7c4630f3d505257fe4525c8d7a438
CernenkoVI <chernenkovi@yahoo.com>
2025-08-14 15:09:39 +0100

    uncommented allauth in settings.py

--- backend/nikabond_backend/nikabond_backend/settings.py
+++ backend/nikabond_backend/nikabond_backend/settings.py
@@ -100,7 +100,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-#    'allauth.account.middleware.AccountMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
